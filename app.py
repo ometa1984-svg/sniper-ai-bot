e24a2fba45913bcdb2783711efb98fe6da1af093
ometa1984-svg <ometa1984@gmail.com>
2026-04-15 05:30:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,12 @@ def data():
     return jsonify(state)
 
 # ================= START ================= #
-if __name__ == "__main__":
-    send("🤖 SNIPER QUANT AI STARTED")
-    threading.Thread(target=run_bot).start()
+)if __name__ == "__main__":
+    import threading
+
+    def start_bot():
+        run_bot()
+
+    threading.Thread(target=start_bot, daemon=True).start()
+
     app.run(host="0.0.0.0", port=10000)
